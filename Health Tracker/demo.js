564cdbeef15165e1d9681020fcf365dadfccd2c6
mohamedwasimm2025eee <mohamedwasim.m2025eee@sece.ac.in>
2026-04-27 10:59:44 +0530

    Add files via upload

---
+++ Health Tracker/demo.js
@@ -0,0 +1,19 @@
+alert("Hello");
+let result1;
+let result2;
+let result;
+function btn1()
+{
+let height=prompt("Enter the Height");
+result1=height;
+}
+function btn2()
+{
+let weight=prompt("Enter the Weight");
+result2=weight;
+}
+function btn()
+{
+result=result1/result2;
+document.write(result);
+}
